785b1dc7f460076f678feddcc1ba1e7277fe6f07
rubences <91731294+rubences@users.noreply.github.com>
2024-02-14 09:30:40 +0000

    arreglos de ejercicios 1 y 2

---
+++ ejercicios/ejercicio2.java
@@ -0,0 +1,11 @@
+public class ejercicio2 {
+    public static void main(String[] args) {
+        char maxChar = Character.MAX_VALUE;
+        short maxShort = Short.MAX_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+
+        System.out.println("El mayor número entero representable en un char es: " + (int) maxChar);
+        System.out.println("El mayor número entero representable en un short es: " + maxShort);
+        System.out.println("El mayor número entero representable en un int es: " + maxInt);
+    }
+}
